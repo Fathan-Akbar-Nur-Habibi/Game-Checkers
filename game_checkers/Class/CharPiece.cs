a695f9916670c65d4bd7a9a6fc20a92e3f16e3e4
Fathan-Akbar-Nur-Habibi <fathanakbarnh@gmail.com>
2024-08-02 14:35:54 +0700

    add design CharPiece

--- game_checkers/Class/CharPiece.cs
+++ game_checkers/Class/CharPiece.cs
@@ -1,30 +1,47 @@
+using System.Text.Json;
+
 namespace game_checkers;
 
 public class CharPiece : Piece
 {
-    public int BaseAbility { get; }
-    public bool WhichAbility { get; }
+	public int BaseAbility { get; }
+	public bool WhichAbility { get; }
+	public static bool TrueKing {get; private set;}
+	static CharPiece()
+	{
+		LoadConfiguration();
+	}
+	
+	private static void LoadConfiguration()
+	{
+		var configText = System.IO.File.ReadAllText("config.json");
+		var config = JsonSerializer.Deserialize<Dictionary<string,bool>>(configText);
+		if (config !=null && config.ContainsKey("trueKing"))
+		{
+			TrueKing = config["trueKing"];
+		}
+	}
 
-    public CharPiece(int id, CharType type, Colour colour, int baseAbility, bool whichAbility)
-        : base(id, type, colour)
-    {
-        BaseAbility = baseAbility;
-        WhichAbility = whichAbility;
-    }
+	public CharPiece(int id, CharType type, Colour colour, int baseAbility, bool whichAbility)
+		: base(id, type, colour)
+	{
+		BaseAbility = baseAbility;
+		WhichAbility = whichAbility;
+	}
 
-    public int GetCurrentAbility()
-    {
-        return BaseAbility + (WhichAbility ? 1 : 0);
-    }
+	public int GetCurrentAbility()
+	{
+		return BaseAbility + (WhichAbility ? 1 : 0);
+	}
 
-    public int GetUpgrade()
-    {
-        return BaseAbility + 1;
-    }
+	public int GetUpgrade()
+	{
+		return BaseAbility + 1;
+	}
 
-    public override List<Destination> AvailableMove()
-    {
-        
-        return new List<Destination>();
-    }
+	public override List<Destination> AvailableMove()
+	{
+		
+		return new List<Destination>();
+	}
 }
